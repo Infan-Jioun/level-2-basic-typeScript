4410ce8382ce10e34c526a9d3e7eab7b2e37168b
Infan-Jioun <infanjiounrahman20606@gmail.com>
2025-11-10 03:04:21 +0600

    spread use for array and object

--- src/spreadAndRest.ts
+++ src/spreadAndRest.ts
@@ -13,7 +13,15 @@ const universityFriend = ["shawn", "maown", "oman"]
 
 // Spread use for Object
 
-const user = { name: "infan", phone: "01610240096", email: "infanjiounrahman20606@gmail.com" }
-const otherInfo = { address: "panchlish, Chattogram", DOB: "December" }
-const userInfo = { ...user, ...otherInfo }
-console.log(userInfo); // data golo eksongge kore rakbe
+// const user = { name: "infan", phone: "01610240096", email: "infanjiounrahman20606@gmail.com" }
+// const otherInfo = { address: "panchlish, Chattogram", DOB: "December" }
+// const userInfo = { ...user, ...otherInfo }
+// console.log(userInfo); // data golo eksongge kore rakbe
+
+// Rest Opreator
+const sendInvite = (...friends: string[]) => {
+    friends.forEach((friend: string) => console.log(`Send Invitation ${friend}`))
+}
+sendInvite("infan", "jioun", "rahman")
+
+//  ekane forEach kora hoise only se array return kore na tai  se only element golo loop  chala 

e542010a098e23458ec98c7ed002d18ca824dcc0
infan11 <infanjiounrahman20606@gmail.com>
2025-11-09 14:57:18 +0600

    added primitive.ts

---
+++ src/premitive.ts
@@ -0,0 +1,11 @@
+// data types
+//Js :  String , number, boolean, undifined, null
+// Extra:  TS  : never, unknown, void 
+let name: string = "infan";
+
+//name = "jioun"; // it is string now
+// name  = 33 // erorr that is number
+
+let isAdmin: boolean = true
+//isAdmin = false  // it is boolean now
+//isAdmin = 3423 // erorr that is number

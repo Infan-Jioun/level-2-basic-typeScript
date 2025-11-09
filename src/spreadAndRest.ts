c5034ff22467633b6d046902c5b005b8bd1db3fe
Infan-Jioun <infanjiounrahman20606@gmail.com>
2025-11-10 02:31:32 +0600

    spread use for array and object

--- src/spreadAndRest.ts
+++ src/spreadAndRest.ts
@@ -1,10 +1,19 @@
+
+// Spread use for array
 const friend = ["infan", "jioun", "rahman"];
-const schoolFriend = ["rahim", "karim", "sarim" ];
+const schoolFriend = ["rahim", "karim", "sarim"];
 const collegeFriend = ["lorim", "torim", "aorim", "harim"]
 const universityFriend = ["shawn", "maown", "oman"]
 
 
-console.log("friend" ,friend.push(...schoolFriend));
-console.log(friend.push(...collegeFriend));
-console.log(friend.push(...universityFriend));
-console.log(friend);
+// console.log("friend", friend.push(...schoolFriend));
+// console.log(friend.push(...collegeFriend));
+// console.log(friend.push(...universityFriend));
+// console.log(friend);
+
+// Spread use for Object
+
+const user = { name: "infan", phone: "01610240096", email: "infanjiounrahman20606@gmail.com" }
+const otherInfo = { address: "panchlish, Chattogram", DOB: "December" }
+const userInfo = { ...user, ...otherInfo }
+console.log(userInfo); // data golo eksongge kore rakbe

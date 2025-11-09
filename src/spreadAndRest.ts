d1b1fe85fdfe42d7a3c7964d1f0e72e9a0a79336
Infan-Jioun <infanjiounrahman20606@gmail.com>
2025-11-10 02:22:29 +0600

    spread use for array

---
+++ src/spreadAndRest.ts
@@ -0,0 +1,10 @@
+const friend = ["infan", "jioun", "rahman"];
+const schoolFriend = ["rahim", "karim", "sarim" ];
+const collegeFriend = ["lorim", "torim", "aorim", "harim"]
+const universityFriend = ["shawn", "maown", "oman"]
+
+
+console.log("friend" ,friend.push(...schoolFriend));
+console.log(friend.push(...collegeFriend));
+console.log(friend.push(...universityFriend));
+console.log(friend);

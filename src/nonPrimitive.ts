44da707eeeb376925b4e242fe0ea40c9c11e0d87
Infan-Jioun <infanjiounrahman20606@gmail.com>
2025-11-09 18:13:07 +0600

    added nonPrimitive useing in the array and tuple

---
+++ src/nonPrimitive.ts
@@ -0,0 +1,10 @@
+// Non Primitves 
+// array, object, touple
+
+let studentName: string[] = ["infan", "jioun", "rahman"]
+studentName.push("jito")
+console.log(studentName);
+
+// tuple 
+let student: [string , number, ] = ["infan" , 22] 
+console.log(student);

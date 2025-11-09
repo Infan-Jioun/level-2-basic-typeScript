6967d075e5849de8a49fe34bae9d313198c5194a
Infan-Jioun <infanjiounrahman20606@gmail.com>
2025-11-10 03:16:26 +0600

    added destrctureing in the usege object destuctureing

---
+++ src/destuctureing.ts
@@ -0,0 +1,25 @@
+// two types destuctureing
+//1. Object destuctureing
+//2. Array destuctureing
+
+// TODO: Object destuctureing
+// interface userInfo {
+//     name: string,
+//     email: string,
+//     phone: number
+// }
+const user = {
+    name: "infan jioun rahman",
+    email: "infanjiounrahman20606@gmail.com",
+    phone: 1610240096,
+    Deatils: {
+        address: "pnachlish, Chattogram",
+        university: "Premier University Chattogram",
+        blood: "AB+"
+    }
+
+}
+const { name: fullName, Deatils: { address } } = user;
+console.log("Name", fullName, "Address:", address);
+
+// name alias name  use fullName 

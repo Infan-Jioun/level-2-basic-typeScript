f8f16bf793ca8d2c85bd10c2e6c3dbae29a19647
Infan-Jioun <infanjiounrahman20606@gmail.com>
2025-11-09 19:38:34 +0600

    added function.js some funtion usege in this filr

--- src/function.ts
+++ src/function.ts
@@ -12,6 +12,7 @@ function addNormal(num1: number, num2: number): number {
 
 // this হলো current object reference।
 
+
 // Method-এর context অনুযায়ী, এটা যেই object-এর ভিতরে method আছে সেটা points করে।
 // TODO object => function => method
 
@@ -24,3 +25,21 @@ const poorUser = {
     }
 }
 console.log(poorUser.addBalance(5000));
+
+
+// 1️⃣ Array & map()
+
+// arr হলো number type এর array [4, 8, 12]
+
+// .map() হলো array method যা প্রতিটি element নিয়ে callback function apply করে।
+
+// .map() একটি নতুন array return করে, original array change করে না।
+// callback funtion
+//এটা arrow function, যেটা .map() এর callback হিসেবে ব্যবহার হয়েছে।
+
+//Parameter element হলো .map() method দ্বারা array-এর প্রতিটি item।
+
+//Function body element * element → element এর square calculate করে।
+const arr: number[] = [4, 8, 12]
+const sqrArray = arr.map((element: number) => element * element)
+console.log(sqrArray);

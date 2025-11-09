11897b614960eaa7627a37b79ba9a59a25b4a0bf
Infan-Jioun <infanjiounrahman20606@gmail.com>
2025-11-09 19:27:23 +0600

    added object funtion method

--- src/function.ts
+++ src/function.ts
@@ -5,5 +5,22 @@ function addNormal(num1: number, num2: number): number {
     return num1 + num2
 }
 // console.log(addNormal(2, 2));
-const addArrow = (num1: number, num2: number): number => num1 + num2
-console.log(addArrow(4, 4));
+// const addArrow = (num1: number, num2: number): number => num1 + num2
+// console.log(addArrow(4, 4));
+
+// 1️⃣ this কী?
+
+// this হলো current object reference।
+
+// Method-এর context অনুযায়ী, এটা যেই object-এর ভিতরে method আছে সেটা points করে।
+// TODO object => function => method
+
+const poorUser = {
+    name: "infan",
+    balance: 50,
+    addBalance(value: number): number {
+        const totalBalance = this.balance + value
+        return totalBalance;
+    }
+}
+console.log(poorUser.addBalance(5000));

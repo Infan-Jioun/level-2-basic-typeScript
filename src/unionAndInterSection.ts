899d3e0589e4dff1294a7d6dcd3013216417e5a4
Infan-Jioun <infanjiounrahman20606@gmail.com>
2025-11-10 15:37:36 +0600

    added unionAndInterSection in the use admin, user example and added intersection &

--- src/unionAndInterSection.ts
+++ src/unionAndInterSection.ts
@@ -1,13 +1,33 @@
 
 /// union  | use 
-type UserRole = "admin" | "user";
-const getUserRole = (role: UserRole) => {
-    if (role === "admin") {
-        return "Admin"
-    } else if (role === "user") {
-        return "User"
-    } else {
-        return "guest"
-    }
+// type UserRole = "admin" | "user";
+// const getUserRole = (role: UserRole) => {
+//     if (role === "admin") {
+//         return "Admin"
+//     } else if (role === "user") {
+//         return "User"
+//     } else {
+//         return "guest"
+//     }
+// }
+// console.log(getUserRole("admin"));
+// intersection &
+type Employee = {
+    id: string;
+    name: string;
+    phoneNo: string
+
 }
-console.log(getUserRole("admin"));
+type Manager = {
+    designation: string;
+    teamsize: number
+}
+type EmployeeAndManager = Employee & Manager;
+const Manager: EmployeeAndManager = {
+    id: "123",
+    name: "Infan",
+    phoneNo: "34235",
+    designation: "manager",
+    teamsize: 40
+}
+console.log(Manager);

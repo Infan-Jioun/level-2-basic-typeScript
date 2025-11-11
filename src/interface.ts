9761a625d3960cf69362e203fc01945185e5946e
Infan-Jioun <infanjiounrahman20606@gmail.com>
2025-11-12 00:20:56 +0600

    added interfaces firstly object and funtion usege

--- src/interface.ts
+++ src/interface.ts
@@ -1,5 +1,6 @@
 
 // Interface ব্যবহার করা হয় object বা class-এর “structure” define করার জন্য, যাতে কোডটা predictable, readable আর type - safe হয়।
+// interface use hoi only object type: array, object, function
 interface User {
     name: string;
     email: string;
@@ -26,3 +27,21 @@ const userDetails: UserInfoWithRole = {
     education: "Premier University Chittagong"
 }
 console.log(userDetails);
+
+
+// function 
+// interface Add {
+//     (num1: number, num2: number): number;
+// }
+
+// const add: Add = (num1, num2) => num1 + num2
+// console.log(add(2, 3));
+
+interface Icart {
+    (price: number, quantity: number): number
+}
+const cartTotal: Icart = (price, quantity) => {
+    return price * quantity
+}
+
+console.log(cartTotal(10, 10));

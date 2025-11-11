e48e46f30512fd5b1ec701e597d28294a9be4f1e
Infan-Jioun <infanjiounrahman20606@gmail.com>
2025-11-11 11:10:19 +0600

    added typeAssertion

--- src/typeAssertion.ts
+++ src/typeAssertion.ts
@@ -1,9 +1,24 @@
 let anything: any;
 anything = 435;
+(anything as number)
 const kgToGMConverter = (input: string | number) => {
-if(typeof input === "number"){
-    return input * 1000
-}
+    if (typeof input === "number") {
+        return input * 1000
+    } else if (typeof input === "string") {
+
+        const [value] = input.split(" ")
+        console.log(value);
+        return `Converted output is ${Number(value)}`
+    }
 }
 
 console.log(kgToGMConverter(2));
+console.log(kgToGMConverter("2 kg"));
+type CustomError = {
+    messege: string
+}
+try {
+
+} catch (err) {
+   console.log((err as CustomError).messege);
+}

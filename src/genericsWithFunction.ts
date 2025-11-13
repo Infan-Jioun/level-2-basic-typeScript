0c7c3bc4efad57733fb1657f9e1143da15c0bc1a
Infan-Jioun <infanjiounrahman20606@gmail.com>
2025-11-13 11:54:18 +0600

    generics dynamic funtion added

--- src/genericsWithFunction.ts
+++ src/genericsWithFunction.ts
@@ -1,15 +1,20 @@
-const createArrayString = (value: string) => [value];
-const createArrayWithNumber = (value: number) => [value]
-const createArrayWithObj = (value: {
-    id: number,
-    name: string
-}) => {
+// const createArrayString = (value: string) => [value];
+// const createArrayWithNumber = (value: number) => [value]
+// const createArrayWithObj = (value: {
+//     id: number,
+//     name: string
+// }) => {
+//     return [value]
+// }
+// generic dynamic funtion 
+const createArrayWithGeneric = <T>(value: T) => {
     return [value]
 }
 
-const arrString = createArrayString("apple");
-const arrNumber = createArrayWithNumber(234);
-const arrObj = createArrayWithObj({
+const arrString = createArrayWithGeneric("apple");
+const arrNumber = createArrayWithGeneric(234);
+const arrObj = createArrayWithGeneric({
     id: 2,
     name: "infan jioun "
-}) 
+})
+console.log(arrString, arrNumber, arrObj);

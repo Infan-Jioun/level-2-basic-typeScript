22f380c89ad4f0f3e2285f42e05e0e1e57555e9a
Infan-Jioun <infanjiounrahman20606@gmail.com>
2025-11-13 12:18:13 +0600

    generics dynamic funtion added

--- src/genericsWithFunction.ts
+++ src/genericsWithFunction.ts
@@ -18,3 +18,35 @@ const arrObj = createArrayWithGeneric({
     name: "infan jioun "
 })
 console.log(arrString, arrNumber, arrObj);
+
+// tuple 
+const createArrayWithTuple = (param1: string, param2: string) => [
+    param1, param2
+]
+const createArrayTupleGeneric = <X, Y>(param1: X, param2: Y) => [
+    param1, param2
+]
+const res1 = createArrayTupleGeneric("Infan", false);
+const res2 = createArrayTupleGeneric(334, { name: "Infan Jioun Rahman" });
+
+const addStudentToCourse = <T>(studentInfo: T) => {
+    return {
+        course: "Next Developent ",
+        ...studentInfo
+    }
+
+}
+const student1 = {
+    id: "1",
+    name: "Infan",
+    email: "infanjiounrahman20606@gmail.com",
+    level_1: false
+}
+const student2 = {
+    id: "2",
+    name: "Jioun",
+    email: "infanjiounrahman8122003@gmail.com",
+    level_1: true
+}
+const result = addStudentToCourse(student1)
+console.log(result);

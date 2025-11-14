64bcf8204f1b38951ae5831fe9df76dc38afc8ad
Infan-Jioun <infanjiounrahman20606@gmail.com>
2025-11-14 09:08:22 +0600

    constraint added

--- src/keyOfConstraint.ts
+++ src/keyOfConstraint.ts
@@ -1,5 +1,5 @@
 type RichPeopleVehicle = {
-    car: string;
+    car: string; // key value
     bike: string;
     cng: string
 }
@@ -7,3 +7,16 @@ type RichPeopleVehicle = {
 type MyVehicle1 = "bike" | "car" | "cng";
 type MyVehicle2 = keyof RichPeopleVehicle;
 const myVehicle: MyVehicle1 = "bike"
+console.log(myVehicle);
+// constraint
+const user = {
+    id: 222,
+    name: "infan",
+    address: {
+        city: "chattogram"
+    }
+}
+const myId = user["id"]
+const name = user["name"]
+const address = user["address"]
+console.log({ myId, name, address });

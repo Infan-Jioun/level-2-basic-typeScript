b493825a55763c66e18e2bf099c55bfd4b881748
Infan-Jioun <infanjiounrahman20606@gmail.com>
2025-11-14 08:49:56 +0600

    added Constrain.ts

--- src/constrain.ts
+++ src/constrain.ts
@@ -1,14 +1,23 @@
-const addStudentToCourse = <T>(studentInfo: T) => {
+type student = {
+    id: number;
+    name: string;
+    dateOfBirth: string;
+    class: string;
+}
+const addStudentToCourse = <T extends student>(studentInfo: T) => {
     return {
         course: "Next Level",
         ...studentInfo
     }
 }
 const student1 = {
-    id: "1",
+    id: 3,
     name: "Infan",
     email: "infanjiounrahman20606@gmail.com",
-    level_1: false
+    level_1: false,
+    dateOfBirth: "23-10-2025",
+    class: "10"
+
 }
 const student2 = {
     id: "2",
@@ -16,6 +25,7 @@ const student2 = {
     email: "infanjiounrahman8122003@gmail.com",
     level_1: true
 }
+
 const result = addStudentToCourse(student1)
 console.log(result)
 

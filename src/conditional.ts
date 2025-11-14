b03f9681011b86f28d23afef40d6c16e5097d4f8
Infan-Jioun <infanjiounrahman20606@gmail.com>
2025-11-14 11:09:12 +0600

    added condiotionl

---
+++ src/conditional.ts
@@ -0,0 +1,10 @@
+type A = null;
+type B = undefined;
+type C = A extends number ? true : B extends undefined ? true : false;
+type RichProplesVelue = {
+    bike: string;
+    car: string;
+    ship: string
+}
+type CheckVehicle<T> = T extends keyof RichProplesVelue ? true : false;
+type HasBike = CheckVehicle<"tractor">

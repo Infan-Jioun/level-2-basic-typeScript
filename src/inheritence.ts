34c66187410e39dcf0ca87d30d1c95db1e494408
Infan-Jioun <infanjiounrahman20606@gmail.com>
2025-11-16 00:34:54 +0600

    added inheritens usege parent

--- src/inheritence.ts
+++ src/inheritence.ts
@@ -1,18 +1,32 @@
-class Student {
+class Parent {
     name: string;
     age: number;
     address: string;
-    constructor(name: string, age: number, address: string) {
-        this.name = name,
-            this.age = age,
-            this.address = address
 
+
+    constructor(name: string, age: number, address: string, designation: string) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
     }
     methodStudent(sleepHour: number) {
         console.log(`${this.name} ${sleepHour} gonta gomai  `);
     }
 }
 
-const student1 = new Student("infan", 22, "bangladesh");
+class Student extends Parent { }
+class Teacher extends Parent {
+    designation: string // property
+    constructor(name: string, age: number, address: string, designation: string) {
+
+        super(name, age, address, designation)
+        this.designation = designation
+    }
+    takeClass(numberOfClass: number) {
+        console.log(`${this.name} class ne ${numberOfClass} ta`);
+    }
+}
+const student1 = new Student("infan", 22, "bangladesh", "Student");
+const Teacher1 = new Teacher("Jioun Sir", 30, "Bangladesh", "Teacher")
 // console.log(student1);
-student1.methodStudent(7)
+Teacher1.takeClass(7)

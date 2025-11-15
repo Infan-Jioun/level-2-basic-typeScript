c2cf5b87bf3267a181418d422eed8d7e85035069
Infan-Jioun <infanjiounrahman20606@gmail.com>
2025-11-15 18:56:09 +0600

    added class.ts 2nd method useing perameter properties

--- src/class.ts
+++ src/class.ts
@@ -1,21 +1,33 @@
-// class
-class Animal {
-    name: string;
-    species: string;
-    sound: string;
+// // class
+// class Animal {
+//     name: string;
+//     species: string;
+//     sound: string;
 
-    constructor(name: string, specias: string, sound: string) {
-        this.name = name,
-            this.species = specias,
-            this.sound = sound
+//     constructor(name: string, specias: string, sound: string) {
+//         this.name = name,
+//             this.species = specias,
+//             this.sound = sound
+//     }
+//     // function
+//     makeSound() {
+//         console.log(`${this.name} is making sound: ${this.sound}`);
+//     }
+// }
+// // object
+// const dog = new Animal("doggy", "dog", "gew gew gew")
+// // console.log({ dog });
+// dog.makeSound();
+
+// 2nd Method useing peramiter properties
+class Animal {
+    constructor(public name: string, public species: string, public sound: string) {
+
     }
-    // function
     makeSound() {
         console.log(`${this.name} is making sound: ${this.sound}`);
     }
 }
 // object
-const dog = new Animal("doggy", "dog", "gew gew gew")
-// console.log({ dog });
-dog.makeSound();
-
+const cat = new Animal("caty", "cat", "mew mew mew")
+cat.makeSound()

30d9a4d5ffb4cce0d6ccbe7a766e199e82c32ef0
Infan-Jioun <infanjiounrahman20606@gmail.com>
2025-11-17 00:01:35 +0600

    added polmorphism.ts

--- src/polmorphism.ts
+++ src/polmorphism.ts
@@ -20,4 +20,39 @@ const person1 = new Person()
 const person2 = new Student()
 const person3 = new NextLevelWebDeveloper()
 
-console.log(getSleepingHours(person3));
+console.log(getSleepingHours(person1));
+
+class Shape {
+    getArea(): number {
+        return 0
+    }
+}
+class Circle extends Shape {
+    radius: number;
+    constructor(radius: number) {
+        super()
+        this.radius = radius
+    }
+    getArea(): number {
+        return Math.PI * this.radius * this.radius;
+    }
+}
+class Reactangle extends Shape {
+    height: number;
+    width: number;
+    constructor(height: number, width: number) {
+        super();
+        this.height = height;
+        this.width = width;
+    }
+    getArea(): number {
+        return this.height * this.width
+    }
+}
+const getArea = (param: Shape) => {
+    console.log(param.getArea());
+}
+const shape1 = new Shape();
+const shape2 = new Circle(10);
+const shape3 = new Reactangle(10, 20);
+getArea(shape2)

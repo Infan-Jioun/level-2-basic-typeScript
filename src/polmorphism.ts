a369ad1390446c1a5bb4abbd74ceb085c479eb72
Infan-Jioun <infanjiounrahman20606@gmail.com>
2025-11-16 23:03:13 +0600

    added polmorphism.ts

--- src/polmorphism.ts
+++ src/polmorphism.ts
@@ -1,15 +1,23 @@
 class Person {
     getSleep() {
-        console.log(`I Sleep for 8 Hours`);
+        console.log(`I am a Normal Happy Person I sleep for 8 hours`);
     }
 }
 class Student extends Person {
     getSleep() {
-        console.log(`I Sleep for 8 Hours`);
+        console.log(`I am a student. I sleep 7 Hours`);
     }
 }
-class Person {
+class NextLevelWebDeveloper extends Person {
     getSleep() {
-        console.log(`I Sleep for 8 Hours`);
+        console.log(`I am Next level web developer .I Sleep for 8 Hours`);
     }
 }
+const getSleepingHours = (param: Person) => {
+    param.getSleep();
+}
+const person1 = new Person()
+const person2 = new Student()
+const person3 = new NextLevelWebDeveloper()
+
+console.log(getSleepingHours(person3));
